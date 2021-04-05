fd0c4c8f01cbbfd30f5c24ba33594b0728e86342
haunguyen140299 <haunguyen140299@gmail.com>
2021-04-05 11:47:44 +0700

    a

---
+++ 4IN1_FrontEnd/FrontEnd/src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DangkitochucsukienComponent } from './dangkitochucsukien/dangkitochucsukien.component';
+import {CKEditorModule} from 'ckeditor4-angular';
+import { DangkithamgiasukienComponent } from './dangkithamgiasukien/dangkithamgiasukien.component';
+import { TrangchuComponent } from './trangchu/trangchu.component';
+import { DangkilamcongtacvienComponent } from './dangkilamcongtacvien/dangkilamcongtacvien.component';
+import { ThongtincanhanComponent } from './thongtincanhan/thongtincanhan.component';
+import { SukienComponent } from './sukien/sukien.component';
+import { DangnhapComponent } from './dangnhap/dangnhap.component';
+import { DangkyComponent } from './dangky/dangky.component';
+import {AdminModule} from './admin/admin.module';
+import { QuanlycongtacvienComponent } from './quanlycongtacvien/quanlycongtacvien.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    DangkitochucsukienComponent,
+    DangkithamgiasukienComponent,
+    TrangchuComponent,
+    DangkilamcongtacvienComponent,
+    ThongtincanhanComponent,
+    SukienComponent,
+    DangnhapComponent,
+    DangkyComponent,
+    QuanlycongtacvienComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CKEditorModule,
+    AdminModule,
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ 4IN1_FrontEnd/FrontEnd/src/app/timkiemsukien/timkiemsukien.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-timkiemsukien',
+  templateUrl: './timkiemsukien.component.html',
+  styleUrls: ['./timkiemsukien.component.scss']
+})
+export class TimkiemsukienComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
